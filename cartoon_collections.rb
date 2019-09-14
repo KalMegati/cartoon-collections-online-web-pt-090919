d7b28ae49029658f46fe53723551702bfb27df19
Srikalyan Megati <kalmegati@gmail.com>
2019-09-14 23:52:18 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,19 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(march)
+  march.each_with_index { |d, i| puts "#{i+1}. #{d}" }
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteers)
+  planeteers.collect { |token| token.capitalize + "!" }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(kiai)
+  kiai.any? { |call| call.length > 4 }
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(assortment)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_types.length.times { |index|
+    return cheese_types[index] if assortment.include?(cheese_types[index])
+  }
+  nil
 end
